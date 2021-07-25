c69692102bcb50b46e65e71431c72c5e95264780
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-07-25 17:45:48 -0300

    Finalizin list server

---
+++ src/components/GuildIcon/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  image: {
+    width: 62,
+    height: 66,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+});
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Components
+import { Appointment } from '../../components/Appointment';
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
+import { ListHeader } from '../../components/ListHeader';
 
 import { styles } from './style';
 import { useState } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { ListDivider } from '../../components/ListDivider';
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendarios',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/05 as 20:31h',
+      description: 'Hoje  e dia de jogar um CSZIn',
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Grand Chase',
+        icon: null,
+        owner: false,
+      },
+      category: '1',
+      date: '22/05 as 20:31h',
+      description: 'Hoje  e dia de jogar um CSZIn',
+    },
+  ];
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -27,6 +58,19 @@ export function Home() {
           categorySelected={category}
           setCategory={handleCategorySelect}
         />
+
+        <View style={styles.content}>
+          <ListHeader title="Partidas agendadas" subtitle="Tota: 2" />
+
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Appointment data={item} />}
+            ItemSeparatorComponent={() => <ListDivider />}
+            style={styles.matches}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
       </View>
     </View>
   );
