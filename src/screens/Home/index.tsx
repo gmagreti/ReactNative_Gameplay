9a57068e07fc10d90526b8babcdd55b809c617da
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-07-25 16:25:36 -0300

    Selected categorys

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ButtonAdd } from '../../components/ButtonAdd';
+
+// Components
 import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from './style';
+import { useState } from 'react';
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
         <Profile />
-
         <ButtonAdd />
+      </View>
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
       </View>
     </View>
   );
